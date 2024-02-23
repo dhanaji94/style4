68e4fe18514212c0d3a470b9be185c8ba714305d
dhanaji94 <118427482+dhanaji94@users.noreply.github.com>
2024-02-23 13:44:58 +0530

    Add files via upload

---
+++ d1.js
@@ -0,0 +1,17 @@
+
+var swiper = new Swiper(".img-box", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
